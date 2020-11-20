656c013578f530846e68ffcfcee4b63677e5fceb
appadm <lusterleung@gitee.com>
2020-11-20 15:22:36 +0800

    init

---
+++ src/Home/data.source.js
@@ -0,0 +1,146 @@
+import React from 'react';
+export const Banner00DataSource = {
+  wrapper: { className: 'banner0' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>
+              <span>
+                <p>现代经营分析平台</p>
+              </span>
+            </span>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+  content: {
+    className: 'banner0-content',
+    children: (
+      <span>
+        <p>数据的游乐园</p>
+      </span>
+    ),
+  },
+  button: { className: 'banner0-button', children: 'Learn More' },
+};
+export const Content00DataSource = {
+  wrapper: { className: 'home-page-wrapper content0-wrapper' },
+  page: { className: 'home-page content0' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [{ name: 'title', children: '产品与服务' }],
+  },
+  childWrapper: {
+    className: 'content0-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>一站式数据智慧大脑</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <p>多维度数据分析与挖掘</p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>一站式业务风险监控</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: '在所有需求配置环节事前风险控制和质量控制能力',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: '一站式数据运营',
+            },
+            {
+              name: 'content',
+              children: '沉淀产品接入效率和运营小二工作效率数据',
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <span>©2020 蓝月亮数据科学&nbsp;All Rights Reserved</span>
+      </span>
+    ),
+  },
+};
